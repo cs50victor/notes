b8ca083f2f0421ca943d5a86c6df02957e1dba6e
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-03-12 11:23:57 -0400

    refactor: switch to bun

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withMDX = require("@next/mdx")();
 
 module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ["tsx", "mdx"],
   experimental: {
     mdxRs: true,
   },
@@ -34,32 +34,5 @@ module.exports = withMDX({
         pathname: "/**",
       },
     ],
-  },
-  headers() {
-    return [
-      {
-        source: "/images/rauchg-3d4cecf.jpg",
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-  redirects() {
-    return [
-      {
-        source: "/essays/:nested*",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/slackin/:nested*",
-        destination: "https://github.com/rauchg/slackin",
-        permanent: true,
-      },
-    ];
   },
 });
